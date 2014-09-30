aab8b5509ed420518ca1a69f4543d95c8d08aad4
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-09-30 13:20:59 -0700

    Update our controller to use the Todos factory

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,42 +1,54 @@
 angular.module('todoController', [])
 
-  .controller('mainController', function($scope, $http) {
+  // inject the Todo service factory into our controller
+  .controller('mainController', function($scope, $http, Todos) {
     $scope.formData = {};
     console.log('Angular is linked up!');
 
+    // GET =====================================================================
     // when landing on the page, get all todos and show them
-    $http.get('/api/todos')
+    Todos.get()
       .success(function(data) {
         $scope.todos = data;
         console.log(data);
       })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+      // .error(function(data) {
+      //   console.log('Error: ' + data);
+      // });
 
+    // CREATE ==================================================================
     // when submitting the add form, send the text to the node API
     $scope.createTodo = function() {
-      $http.post('/api/todos', $scope.formData)
-        .success(function(data) {
-          $scope.formData = {}; // clear the form so our user is ready to enter another
-          $scope.todos = data;
-          console.log(data);
-        })
-        .error(function(data) {
-          console.log('Error: ' + data);
-        });
+
+      if (!$.isEmptyObject($scope.formData)) {
+
+        Todos.create($scope.formData)
+          .success(function(data) {
+            $scope.formData = {}; // clear the form so our user is ready to enter another
+            $scope.todos = data;
+            console.log(data);
+          })
+          // .error(function(data) {
+          //   console.log('Error: ' + data);
+          // });
+
+      }
+
     };
 
+    // DELETE ==================================================================
     // delete a todo after checking it
     $scope.deleteTodo = function(id) {
-      $http.delete('/api/todos/' + id)
+
+      Todos.delete(id)
       .success(function(data) {
         $scope.todos = data;
         console.log(data);
       })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+      // .error(function(data) {
+      //   console.log('Error: ' + data);
+      // });
+
     };
 
   });
